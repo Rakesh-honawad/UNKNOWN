6a71e486a546ed94da41b632e832dad602ce79cc
Rakesh Honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-02-26 14:03:58 +0530

    Create 3636-CheckBalancedString

---
+++ 3636-CheckBalancedString/3636-CheckBalancedString.java
@@ -0,0 +1,15 @@
+// Last updated: 26/02/2026, 14:03:58
+class Solution {
+    public boolean isBalanced(String num) {
+        int even =0; int odd=0;
+        for(int i=0;i<num.length();i++){
+            if(i%2==0){
+                even+=num.charAt(i) - '0';
+            }
+            else{
+                odd+=num.charAt(i)- '0';
+            }
+        }
+        return even==odd;
+    }
+}
