28ba3277569433e77b538ae5a266603093c333ea
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-04-19 03:10:15 +0530

    Create 496-NextGreaterElementI

---
+++ 496-NextGreaterElementI/version/java/496-NextGreaterElementI_20260419_031014.java
@@ -0,0 +1,28 @@
+// Last updated: 19/04/2026, 03:10:14
+1class Solution {
+2    public boolean lemonadeChange(int[] bills) {
+3        int five=0;
+4        int ten=0;
+5        for(int i=0;i<bills.length;i++){
+6            if(bills[i]==5) five++;
+7            else if(bills[i]==10){
+8                five--;
+9                ten++;
+10            }
+11          
+12           else{
+13               if(ten>0){
+14                ten--;
+15                five--;
+16               }
+17               else{
+18                   five-=3;
+19               }
+20           }  
+21           if(five<0) return false;
+22
+23        }
+24        return true;
+25        
+26    }
+27}
