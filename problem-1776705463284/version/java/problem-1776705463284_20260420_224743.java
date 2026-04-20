97be4f0875b5094faa10b0df1834df34d8a6be26
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-04-20 22:47:44 +0530

    Create

---
+++ problem-1776705463284/version/java/problem-1776705463284_20260420_224743.java
@@ -0,0 +1,18 @@
+// Last updated: 20/04/2026, 22:47:43
+1class Solution {
+2    public int earliestFullBloom(int[] plantTime, int[] growTime) {
+3        int[][] plants= new int[plantTime.length][2];
+4        for(int i=0;i<plantTime.length;i++){
+5            plants[i][0]=plantTime[i];
+6            plants[i][1]=growTime[i];
+7        }
+8        Arrays.sort(plants, (a, b) -> Integer.compare(b[1], a[1]));
+9        int cur=0;
+10        int total=0;
+11        for(int[] p:plants){
+12            cur+=p[0];
+13            total=Math.max(total,cur+p[1]);
+14        }
+15        return total;
+16    }
+17}
