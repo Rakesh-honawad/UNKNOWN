7125174d58960fcd611b86372803c7547f5dd8a1
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-04-23 04:32:34 +0530

    Create

---
+++ problem-1776898954460/version/java/problem-1776898954460_20260423_043234.java
@@ -0,0 +1,14 @@
+// Last updated: 23/04/2026, 04:32:34
+1class Solution {
+2    public int maxSubArray(int[] nums) {
+3        int max=nums[0];
+4        int curr=0;
+5        for(int num:nums){
+6            curr+=num;
+7            max=Math.max(max,curr);
+8            if(curr<=0)curr=0;;
+9        }
+10        return max;
+11                 
+12    }
+13}
