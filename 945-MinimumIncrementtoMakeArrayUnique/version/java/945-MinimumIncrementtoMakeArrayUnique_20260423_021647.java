f4a80d083c638692a346d4772fc209ac5af5265d
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-04-23 02:16:47 +0530

    Create 945-MinimumIncrementtoMakeArrayUnique

---
+++ 945-MinimumIncrementtoMakeArrayUnique/version/java/945-MinimumIncrementtoMakeArrayUnique_20260423_021647.java
@@ -0,0 +1,30 @@
+// Last updated: 23/04/2026, 02:16:47
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int minIncrement = 0;
+        for(int n: nums){
+            max = Math.max(max,n);
+        }
+
+        int[] freq = new int[nums.length + max];
+        for(int val: nums){
+            freq[val]++;
+        }
+
+        for(int i = 0; i < freq.length; i++){
+            if(freq[i] <= 1){
+                continue;
+            }
+            else{
+                int duplicates = freq[i] - 1;
+                freq[i+1] += duplicates;
+                freq[i] = 1;
+                minIncrement += duplicates;
+            }
+        }
+
+        return minIncrement;
+        
+    }
+}
