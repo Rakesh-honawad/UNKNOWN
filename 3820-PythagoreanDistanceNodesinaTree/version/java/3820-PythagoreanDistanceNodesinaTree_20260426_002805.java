24dd0e657ec3b77e6e81ea3215fe459fd61292c5
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-04-26 00:28:06 +0530

    Create 3820-PythagoreanDistanceNodesinaTree

---
+++ 3820-PythagoreanDistanceNodesinaTree/version/java/3820-PythagoreanDistanceNodesinaTree_20260426_002805.java
@@ -0,0 +1,25 @@
+// Last updated: 26/04/2026, 00:28:05
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public int minDepth(TreeNode root) {
+18        if(root==null) return 0;
+19        int left=minDepth(root.left);
+20        int right=minDepth(root.right);
+21        if(root.left==null||root.right==null)return left+right+1;
+22        return Math.min(left,right)+1;
+23    }
+24}
