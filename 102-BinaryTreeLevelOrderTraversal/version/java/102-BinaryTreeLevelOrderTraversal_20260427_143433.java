60ec4ce9e9bef71a44e00d1e7890f5d0111c45e6
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-04-27 14:34:33 +0530

    Create 102-BinaryTreeLevelOrderTraversal

---
+++ 102-BinaryTreeLevelOrderTraversal/version/java/102-BinaryTreeLevelOrderTraversal_20260427_143433.java
@@ -0,0 +1,29 @@
+// Last updated: 27/04/2026, 14:34:33
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public int kthSmallest(TreeNode root, int k) {
+18        List<Integer>lst=new ArrayList<>();
+19        func(root,lst);
+20        return lst.get(k-1);
+21    }
+22    private void func(TreeNode root, List<Integer> lst){
+23        if(root==null)return;
+24        func(root.left,lst);
+25        lst.add(root.val);
+26        func(root.right,lst);
+27    }
+28}
