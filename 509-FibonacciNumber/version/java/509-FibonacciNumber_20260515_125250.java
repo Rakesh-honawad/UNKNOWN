049aed1ef5485f591b48273b3e099ffeecff2b21
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-05-15 12:52:51 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/version/java/509-FibonacciNumber_20260515_125250.java
@@ -0,0 +1,10 @@
+// Last updated: 15/05/2026, 12:52:50
+1class Solution {
+2    public int fib(int n) {
+3        int[] dp= new int[n+1];
+4        if(n<=1)return n;
+5        if(dp[n]==n)return dp[n];
+6        return dp[n]=fib(n-1)+fib(n-2);
+7        
+8    }
+9}
