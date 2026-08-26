0242c79218038f4dba4d7817bb6d82cd36973593
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-08-26 19:31:48 +0530

    Create 3546-EqualSumGridPartitionI

---
+++ 3546-EqualSumGridPartitionI/version/java/3546-EqualSumGridPartitionI_20260826_193147.java
@@ -0,0 +1,18 @@
+// Last updated: 26/08/2026, 19:31:47
+1class Solution {
+2    public int pivotIndex(int[] nums) {
+3        int res=0;
+4        for(int i=0;i<nums.length;i++){
+5            res+=nums[i];
+6        }
+7
+8        int left=0;
+9        for(int i=0;i<nums.length;i++){
+10            int right=res-left-nums[i];
+11            if(left==right)return i;
+12            left+=nums[i];
+13        }
+14        return -1;
+15
+16    }
+17}
