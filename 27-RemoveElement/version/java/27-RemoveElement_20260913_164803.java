6d639b83de80cb13f1a95ac84b3a0aa3ef6b118c
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-09-13 16:48:03 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/version/java/27-RemoveElement_20260913_164803.java
@@ -0,0 +1,16 @@
+// Last updated: 13/09/2026, 16:48:03
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3        HashMap<Integer,Integer> mp= new HashMap<>();
+4        for(int num:nums){
+5            mp.put(num, mp.getOrDefault(num,0)+1);
+6        }
+7        int n=nums.length/2;
+8        for(int k:nums){
+9            if(mp.get(k)>n){
+10                return k;
+11            }
+12        }
+13        return -1;
+14    }
+15}
