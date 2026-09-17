168938936536df2bc98d3968a589587e14b5dae9
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-09-18 00:20:53 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/version/java/55-JumpGame_20260918_002052.java
@@ -0,0 +1,15 @@
+// Last updated: 18/09/2026, 00:20:52
+1class Solution {
+2    public int findContentChildren(int[] g, int[] s) {
+3        Arrays.sort(g);
+4        Arrays.sort(s);
+5        int i=0;int j=0;
+6        while(j<s.length&& i<g.length){
+7            if(g[i]<=s[j]){
+8                i++;
+9            }j++;
+10        }
+11        return i;
+12        
+13    }
+14}
