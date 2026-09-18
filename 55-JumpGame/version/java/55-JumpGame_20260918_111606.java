2587dcfba6c0457c7c8b16fcfa47cea165255661
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-09-18 11:16:07 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/version/java/55-JumpGame_20260918_111606.java
@@ -0,0 +1,11 @@
+// Last updated: 18/09/2026, 11:16:06
+1class Solution {
+2    public boolean canJump(int[] nums) {
+3        int reachable=0;
+4        for(int i=0;i<nums.length;i++){
+5            if(i>reachable)return false;
+6            reachable=Math.max(reachable,i+nums[i]);
+7        }
+8        return true;
+9    }
+10}
