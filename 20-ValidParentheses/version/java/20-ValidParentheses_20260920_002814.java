c88d09d699ff181fcc4b37fc751ffd4c96f3c502
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-09-20 00:28:15 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/version/java/20-ValidParentheses_20260920_002814.java
@@ -0,0 +1,19 @@
+// Last updated: 20/09/2026, 00:28:14
+1class Solution {
+2    public boolean isValid(String s) {
+3        Stack<Character>st= new Stack<>();
+4        for(char ch:s.toCharArray()){
+5            if(ch=='('||ch=='{'||ch=='['){
+6                st.push(ch);
+7            }else{
+8                if(st.isEmpty())return false;
+9                char top=st.pop();
+10                if(ch==')'&& top!='(')return false;
+11                if(ch=='}'&& top!='{')return false;
+12                if(ch==']'&& top!='[')return false;
+13            }
+14        }
+15        return st.isEmpty();
+16        
+17    }
+18}
