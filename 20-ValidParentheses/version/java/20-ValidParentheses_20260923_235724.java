db5147ec7d46149d8a2649d7450968cfc43eb072
Rakesh-honawad <164733205+Rakesh-honawad@users.noreply.github.com>
2026-09-23 23:57:24 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/version/java/20-ValidParentheses_20260923_235724.java
@@ -0,0 +1,15 @@
+// Last updated: 23/09/2026, 23:57:24
+1class Solution {
+2    public int removeDuplicates(int[] nums) {
+3        int i=0;
+4        int j=1;
+5        while(j<nums.length){
+6            if(nums[i]!=nums[j]){
+7                i++;
+8                nums[i]=nums[j];
+9            }
+10            j++;
+11        }
+12        return i+1;
+13    }
+14}
